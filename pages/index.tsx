fc7f514dc7af3cb154e8af75e8e3a008f3e9adb7
Atul Singh <atulkumar2316singh@gmail.com>
2022-04-17 08:32:39 +0530

    Organized the file using the installed organize package.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
+import { Alert, Box } from "@chakra-ui/react";
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import { Box, Alert } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   return (
